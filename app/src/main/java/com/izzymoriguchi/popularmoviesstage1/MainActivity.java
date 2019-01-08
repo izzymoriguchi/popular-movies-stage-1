0da85a86eda54c39927576e3148b05f0b731ae95
Izzy Moriguchi <izumimoriguchi@gmail.com>
2019-01-08 14:51:40 -0800

    Update MainActivity.java
    Add RecyclerView, Adapter, GridLayoutManager and wire them up.

--- app/src/main/java/com/izzymoriguchi/popularmoviesstage1/MainActivity.java
+++ app/src/main/java/com/izzymoriguchi/popularmoviesstage1/MainActivity.java
@@ -2,12 +2,28 @@ package com.izzymoriguchi.popularmoviesstage1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int NUM_LIST_ITEMS = 100;
+
+    private MovieAdapter adapter;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.rv_movies);
+
+        GridLayoutManager layoutManager = new GridLayoutManager(this, 3);
+        recyclerView.setLayoutManager(layoutManager);
+
+        recyclerView.setHasFixedSize(true);
+
+        adapter = new MovieAdapter(NUM_LIST_ITEMS);
+        recyclerView.setAdapter(adapter);
     }
 }
